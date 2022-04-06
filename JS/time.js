cc0f973022216bfe5c0b28dfee755d713462bcfe
prembhimavat <prem.bhimavat32@gmail.com>
2022-04-06 18:20:22 +0530

    1.1.2

--- JS/time.js
+++ JS/time.js
@@ -1,2 +1,22 @@
-var dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleString();
+// function realTimeClock(){
+
+//     var dt = new Date();
+
+//     var hours = dt.getHours();
+//     var minutes = dt.getMinutes();
+//     var seconds = dt.getSeconds();
+
+//     var aMpM = (hours < 12 ) ? "AM" : "PM";
+
+//     hours = (hours>12)? (hours-12):hours;
+
+//     document.getElementById("datetime").innerHTML = hours + ":" + minutes + ":" + seconds + aMpM;
+    
+//     var t = setTimeout(realTimeClock,500);
+// }
+
+function realTimeClock(){
+    var dt = new Date();
+    document.getElementById("datetime").innerHTML = dt.toLocaleString();
+    var t = setTimeout(realTimeClock,500);
+}
